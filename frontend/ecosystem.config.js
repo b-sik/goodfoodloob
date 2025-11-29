b6efbfa760d4d8c8e38ede507c80ca259fa7c464
b-sik <brian@bsik.net>
2025-11-29 12:58:57 -0500

    pm2 config

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [
+        {
+            name: 'gfl',
+            script: 'pnpm',
+            args: 'run start',
+            interpreter: '/bin/bash',
+        },
+    ],
+};
